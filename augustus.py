6cbf423fe4e1cbc25953b71faf1baa3f054bab5d
Saideep Gona <gona.saideep1@gmail.com>
2018-04-17 18:59:20 -0400

    Modified to allow for hint-based annotation

--- augustus.py
+++ augustus.py
@@ -13,7 +13,7 @@ import multiprocessing
 
 cwd = os.getcwd()
 transcriptome_path = cwd + "/step1.1/intermediate_fasta.fasta"
-
+reference_directory = cwd + "/references/"
 intermediate_dir = cwd + "/step1.2/"
 
 try:
@@ -21,12 +21,24 @@ try:
 except:
     pass
 
+reference = sys.argv[1]
+ref_path = reference_directory + reference
+
+# blat -minIdentity=92 genome.fa cdna.fa cdna.psl
+# blat2hints.pl --in=/step1.2/cdna.psl --out=step1.2/hints.E.gff
+# augustus --species=human --hintsfile=hints.E.gff --extrinsicCfgFile=extrinsic.ME.cfg genome.fa
+
+blat_1 = "blat -minIdentity=92 '"+ref_path+"' '"+transcriptome_path+"' '"+intermediate_dir+"transcriptome.psl'"
+blat_2 = "blat2hints.pl --in='"+intermediate_dir+"transcriptome.psl'"+" "+"--out='"+intermediate_dir+"hints.gff'"
+
 def run_single_augustus(transcriptome_path, species, output_file):
 
     augustus_args = [
         "augustus",
         "--species=" + species,
-        transcriptome_path
+        "--hintsfile=" +intermediate_dir+"hints.gff",
+        "--extrinsicCfgFile=extrinsic.ME.cfg",
+        ref_path
     ]
 
     out = open(intermediate_dir + output_file,"w")
@@ -34,8 +46,14 @@ def run_single_augustus(transcriptome_path, species, output_file):
     augustus = subprocess.Popen(augustus_args, stdout=out)
 
     return
-
+print("Starting Step 1.2 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print("Running Blat Transcriptome Aligner, Outputting .psl")
+os.system(blat_1)
+print("Generating Rnaseq Hints with blat2hints.pl, Outputting hints.gff")
+os.system(blat_2)
+print("Annotating Genome with Augustus, Outputting .gff")
 run_single_augustus(transcriptome_path, 'human', 'augustus_gff.gff')
+print("Step 1.2 Complete >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
 
 
--- raw_rna_seq_to_augustus.py
+++ raw_rna_seq_to_augustus.py
@@ -4,6 +4,7 @@ import sys
 import os
 import subprocess
 import multiprocessing
+import glob
 
 # Inputs
     # reference_genome: filename of reference genome
@@ -17,12 +18,13 @@ import multiprocessing
 #  - Samtools
 #  - Scallop
 #  - Gffread
+#  - blat
 #  - Augustus
 
 
 reference_genome = sys.argv[1]                 # Name of reference genome - eg. hg38.fa -
-input_fastqs = sys.argv[2:-1]               # Names of input fastqs
-cores = str(multiprocessing.cpu_count())
+input_fastqs = sys.argv[2:]               # Names of input fastqs
+cores = str(multiprocessing.cpu_count()-1)
 
 
 print("Number of cpus: ", cores)
@@ -30,6 +32,10 @@ print("Number of cpus: ", cores)
 cwd = os.getcwd()
 reference_directory = cwd + "/references/"
 fastq_directory = cwd + "/fastq/"
+# if not os.path.isdir(cwd + fastq_directory):
+#     print("no fastq directory")
+#     sys.exit()
+
 ref_genome = reference_directory + reference_genome
 
 intermediate_dir = cwd + "/step1.1/"
@@ -38,6 +44,12 @@ try:
 except:
     pass
 
+input_fastqs = [fastq_directory + fastq  for fastq in input_fastqs]
+print(input_fastqs)
+# input_fastqs = glob.glob(fastq_directory + "*.fastq")
+# if len(input_fastqs) == 0:
+#     print("no input fastqs")
+#     sys.exit()
 
 # Arguments for bwa alignment call
 bwa_args = [
@@ -58,6 +70,8 @@ sam_sort_args = [
     intermediate_dir + "intermediate_bam.bam"
 ]
 
+alignment_full = "bwa mem -t "+cores+ref_genome+" ".join(input_fastqs)+"|samtools sort -O BAM -o "+intermediate_dir+"intermediate_bam.bam"
+print(alignment_full)
 # Arguments for scallop assembly call
 scallop_args = [
     "scallop",
@@ -78,8 +92,9 @@ gffread_args = [
 ]
 
 
-print("Starting Pipeline")
-print("Mapping Reads to Reference")
+print("Starting Pipeline ***************************************************************************")
+print("Starting Step 1.1 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print("Mapping Reads to Reference, Outputting .bam")
 
 bwa_error = open(intermediate_dir + "bwa_error.txt", 'w')
 print(bwa_args)
@@ -92,6 +107,18 @@ bwa.stdout.close()
 produce_bam = sam_sort.communicate()
 bwa.wait()
 
-scallop = subprocess.Popen(scallop_args)
+print('Constructing Transcriptome From Mapped Reads, Outputting .gff')
 
-gffread_args = subprocess.Popen(gffread_args)
+scallop = subprocess.run(scallop_args)
+
+print('Constructing .fasta From Transcriptome')
+
+gffread_args = subprocess.run(gffread_args)
+
+print('Step 1.1 Complete >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+
+ #Modify intermediate_fasta.fasta to have chromosome names
+
+
+
+
